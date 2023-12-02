e3b8d8cbc672e00b84a396d220155e6e321b63a4
Pluto <56084131+Mahabalesh-H@users.noreply.github.com>
2023-12-02 14:40:32 +0530

    profile.php

--- PHP/profile.php
+++ PHP/profile.php
@@ -1,27 +1,49 @@
 <?php
+
+// Create connection
 $conn = new mysqli('localhost', 'root', '', 'assignment');
 
+// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-if ($_SERVER["REQUEST_METHOD"] == "GET") {
-    $email = $_GET["email"];
-    $sql = "SELECT * FROM user WHERE email = ?";
-    $stmt = $conn->prepare($sql);
-    $stmt->bind_param("s", $email);
-    $stmt->execute();
-    $result = $stmt->get_result();
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Retrieve user input
+    $firstName = $_POST["firstName"];
+    $lastName = $_POST["lastName"];
+    $email = $_POST["email"];
+    $phoneNumber = $_POST["phoneNumber"];
+    $password = $_POST["password"];;
 
-    if ($result->num_rows > 0) {
-        $user = $result->fetch_assoc();
-        echo json_encode($user);
+    // Check if email or phone number already exists
+    $checkQuery = "SELECT * FROM user WHERE email = ? OR phoneNumber = ?";
+    $checkStmt = $conn->prepare($checkQuery);
+    $checkStmt->bind_param("si", $email, $phoneNumber);
+    $checkStmt->execute();
+    $checkResult = $checkStmt->get_result();
+
+    if ($checkResult->num_rows > 0) {
+        // User already exists
+        echo "User with the same email or phone number already exists!";
     } else {
-        // Return a JSON response indicating that the user was not found
-        echo json_encode(["error" => "User not found"]);
+        // Insert new user data
+        $insertQuery = "INSERT INTO user (firstName, lastName, email, phoneNumber, password) VALUES ( ?, ?, ?, ?, ?)";
+        $insertStmt = $conn->prepare($insertQuery);
+        $insertStmt->bind_param("sssis", $firstName, $lastName, $email, $phoneNumber, $password);
+
+        if ($insertStmt->execute()) {
+            // Registration successful
+            echo "Registration successful!";
+        } else {
+            // Error during registration
+            echo "Error during registration: " . $insertStmt->error;
+        }
+
+        $insertStmt->close();
     }
 
-    $stmt->close();
+    $checkStmt->close();
 }
 
 $conn->close();
