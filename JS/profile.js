52fb0370ec8993e10bd1dc0ec7c4801eb26eb400
Pluto <56084131+Mahabalesh-H@users.noreply.github.com>
2023-12-02 14:38:37 +0530

    profile.js

--- JS/profile.js
+++ JS/profile.js
@@ -1,24 +1,31 @@
 // Assume you have stored the user's email in local storage during login
 var userEmail = localStorage.getItem(userEmail);
 
-// Fetch user details based on email
-$.ajax({
-    type: 'GET',
-    url: '/Guvi/php/profile.php',
-    data: { email: userEmail },
-    success: function (response) {
-        var user = JSON.parse(response);
+$(document).ready(function () {
+    // Assume you have stored the user's email in local storage during login
+    var userEmail = localStorage.getItem("userEmail");
 
-        // Update the HTML with user details
-        $('#firstName').text(user.firstName);
-        $('#lastName').text(user.lastName);
-        $('#address').text(user.address);
-        $('#postCode').text(user.postCode);
-        $('#country').text(user.country);
-        $('#email').text(user.email);
-        $('#phoneNumber').text(user.phoneNumber);
-    },
-    error: function (xhr, status, error) {
-        console.error('AJAX Error:', status, '-', error);
-    }
+    // Fetch user details based on email
+    $.ajax({
+        type: 'GET',
+        url: '/Guvi/php/profile.php',
+        data: { email: userEmail },
+        success: function (response) {
+            var user = JSON.parse(response);
+
+            if (user.error) {
+                // Handle the case where the user is not found
+                console.error('User not found:', user.error);
+            } else {
+                // Update the HTML with user details
+                $('#firstName').text(user.firstName);
+                $('#lastName').text(user.lastName);
+                $('#email').text(user.email);
+                $('#phoneNumber').text(user.phoneNumber);
+            }
+        },
+        error: function (xhr, status, error) {
+            console.error('AJAX Error:', status, '-', error);
+        }
+    });
 });
